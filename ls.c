7e6fd20e9b81dda4607d9fdd30246aa76fb7e0f8
Farid Mammadov <faridmmmdv@gmail.com>
2017-02-19 00:07:33 +0400

    fixed printing style, changed alphasort to comparator

--- ls.c
+++ ls.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
+#include <string.h>
 
-int main(void) {
+#define ORDER 1
+
+int comparator(const struct dirent **a, const struct dirent **b) {
+  return ORDER * strcmp((*a)->d_name, (*b)->d_name);
+}
+
+int main() {
    struct dirent **namelist;
-   int n = scandir(".", &namelist, 0, alphasort);
+   int n = scandir(".", &namelist, 0, comparator);
    int p = 0;
 
    if (n < 0)
        perror("scandir");
-   else
+   else {
         while (p < n){
-          printf("%s\n", namelist[p]->d_name);
+          printf("%s  ", namelist[p]->d_name);
           p++;
         }
+
+        printf("\n");
+    }
 }
