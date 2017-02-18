f1e23260becfa136ef44c7bacc1229c9c2a5791f
Farid Mammadov <faridmmmdv@gmail.com>
2017-02-19 01:06:37 +0400

    added -A, -a, -r flags

--- ls.c
+++ ls.c
@@ -4,6 +4,8 @@
 #include <string.h>
 
 int ORDER = 1;
+int SKIPDOTS = 1;
+int SKIPPARENT = 1;
 
 int comparator(const struct dirent **a, const struct dirent **b) {
   return ORDER * strcasecmp((*a)->d_name, (*b)->d_name);
@@ -12,8 +14,20 @@ int comparator(const struct dirent **a, const struct dirent **b) {
 int main(int argc, char *argv[]) {
    struct dirent **namelist;
 
-   if (argc == 2 && *argv[1] == 'r')
-       ORDER = -1;
+   if (argc == 2){
+     switch(*argv[1]){
+       case 'r':
+         ORDER = -1;
+         break;
+       case 'A':
+         SKIPDOTS = 0;
+         break;
+       case 'a':
+         SKIPDOTS = 0;
+         SKIPPARENT = 0;
+         break;
+     }
+   }
 
    int n = scandir(".", &namelist, 0, comparator);
    int p = 0;
@@ -21,8 +35,20 @@ int main(int argc, char *argv[]) {
    if (n < 0)
        perror("scandir");
    else {
+        if (SKIPPARENT && ORDER == 1)
+          p = 2;
+        else if (SKIPPARENT && ORDER == -1)
+          n -= 2;
+
+
         while (p < n){
-          printf("%s  ", namelist[p]->d_name);
+          char *name = (namelist[p]->d_name);
+          if (SKIPDOTS){
+            if (name[0] != '.')
+              printf("%s  ", name);
+          } else
+              printf("%s  ", name);
+
           p++;
         }
 
