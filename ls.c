8a12d546b533bed73daca6b24cad9fa404608de3
Farid Mammadov <faridmmmdv@gmail.com>
2017-02-19 00:16:09 +0400

    added reverse printing: ./ls r

--- ls.c
+++ ls.c
@@ -3,14 +3,18 @@
 #include <dirent.h>
 #include <string.h>
 
-#define ORDER 1
+int ORDER = 1;
 
 int comparator(const struct dirent **a, const struct dirent **b) {
   return ORDER * strcmp((*a)->d_name, (*b)->d_name);
 }
 
-int main() {
+int main(int argc, char *argv[]) {
    struct dirent **namelist;
+
+   if (argc == 2 && *argv[1] == 'r')
+       ORDER = -1;
+
    int n = scandir(".", &namelist, 0, comparator);
    int p = 0;
 
