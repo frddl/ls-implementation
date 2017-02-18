6c21b9d2bf29ddac6254bac175f2f11dc85ca177
Farid Mammadov <faridmmmdv@gmail.com>
2017-02-18 23:52:21 +0400

    listing files in current dir

---
+++ ls.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int main(void) {
+   struct dirent **namelist;
+   int n = scandir(".", &namelist, 0, alphasort);
+   if (n < 0)
+       perror("scandir");
+   else
+        while (n--)
+          printf("%s\n", namelist[n]->d_name);
+}
